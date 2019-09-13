7499719a57b8f5f90dad1eb711ba0798cfaffc5d
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-13 09:43:36 -0600

    Created first table for projects. Working on task table

--- data/migrations/20190913091852_bootstrap.js
+++ data/migrations/20190913091852_bootstrap.js
@@ -1,8 +1,56 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('project', tbl => {
+            //PK
+            tbl.increments()
+
+            //name ---> required
+            tbl
+                .string('project_name', 255)
+                .notNullable()
+
+            //description
+            tbl
+                .string('project_name', 516)
+                .notNullable()
+
+            //boolean indicates it project is complete or not, default to false ---> required
+            tbl
+                .boolean('completed')
+                .default(false)
+
+        })
+
+        .createTable('resource', tbl => {
+            //PK
+            tbl.increments()
+
+            //name ---> required
+            //description
+        })
+
+        .createTable('task', tbl => {
+            //FK
+            tbl
+                .integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+
+            //description what needs to be done ---> required
+            //notes for additional info
+            //boolean indicates it project is complete or not, default to false ---> required
+
+        })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('task')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('project')
 };
--- notes.md
+++ notes.md
@@ -9,3 +9,8 @@
 - when adding a `task` the client must provide a description, the notes are optional.
 - when adding a `task` the client must provide the `id` of an existing project.
 - for `projects` and `tasks` if no value is provided for the `completed` property, the API should provide a default value of `false`
+
+- project O --------- M tasks
+- tasks O --------- O Project
+- project O -------- M Resource
+- resource O ------- M Projects
