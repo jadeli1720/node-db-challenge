a0c204b81fccfc8b1ca5913152fd54308e5aa267
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-13 09:53:05 -0600

    Completed all migrations with no errors so far

--- data/migrations/20190913091852_bootstrap.js
+++ data/migrations/20190913091852_bootstrap.js
@@ -7,12 +7,12 @@ exports.up = function (knex) {
 
             //name ---> required
             tbl
-                .string('project_name', 255)
+                .string('project_name', 128)
                 .notNullable()
 
             //description
             tbl
-                .string('project_name', 516)
+                .string('description', 255)
                 .notNullable()
 
             //boolean indicates it project is complete or not, default to false ---> required
@@ -26,24 +26,52 @@ exports.up = function (knex) {
             //PK
             tbl.increments()
 
-            //name ---> required
-            //description
-        })
-
-        .createTable('task', tbl => {
             //FK
             tbl
                 .integer('project_id')
                 .unsigned()
                 .notNullable()
                 .references('id')
-                .inTable('projects')
+                .inTable('project')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+
+            //name ---> required
+            tbl
+                .string('resource_name', 128)
+                .notNullable()
+            //description
+            tbl
+                .string('description', 255)
+                .notNullable()
+        })
+
+        .createTable('task', tbl => {
+            //PK
+            tbl.increments()
+            //FK
+            tbl
+                .integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('project')
                 .onDelete('CASCADE')
                 .onUpdate('CASCADE');
 
             //description what needs to be done ---> required
+            tbl
+                .string('description', 255)
+                .notNullable()
+
             //notes for additional info
+            tbl
+                .string('notes')
+
             //boolean indicates it project is complete or not, default to false ---> required
+            tbl
+                .boolean('completed')
+                .default(false)
 
         })
 };
