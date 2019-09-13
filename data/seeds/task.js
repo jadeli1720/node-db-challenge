307f1d8f5b4fbfca6140ebb2cf8056495c6ab0cd
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-13 10:12:46 -0600

    Completed tasks seed

---
+++ data/seeds/task.js
@@ -0,0 +1,32 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('task').insert([
+    {
+      project_id: 1,
+      description: "Create a forked copy of this project.",
+      notes: "Clone this repo: https://github.com/LambdaSchool/node-db-challenge.git"
+    },
+    {
+      project_id: 1,
+      description: "Install Dependencies",
+      notes: "Don't forget to cd within the folder before adding package.json and npm install"
+    },
+    {
+      project_id: 1,
+      description: "Work with knex to create data schema",
+      notes: "Make sure to install knex and use migrations and seed data"
+    },
+    {
+      project_id: 2,
+      description: "Sweep Tile floors",
+      notes: "Need to sweep kitchen, bathroom, and laundry floors"
+    },
+    {
+      project_id: 2,
+      description: "Vacuum floor",
+      notes: "Don't forget stairs"
+    },
+  ])
+    
+};
